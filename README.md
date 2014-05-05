688510e0cdd9de7b454996f02029b7fdffc69b3b
Michael Savage <mikejsavage@gmail.com>
2014-05-05 17:27:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+This repository contains a single C header file which defines
+`explicit_bzero` and `timingsafe_bcmp` with pragmas to ensure calls to
+them are not optimised out of the program or otherwise.
+
+It is currently only implemented for GCC >= 4.4 and should fail to
+compile with other compilers.
+
+
+Example usage
+--------------
+
+	#include "safebfuns.h"
+	
+	int main() {
+		char b1[ 128 ];
+		char b2[ 128 ];
+	
+		explicit_bzero( b1, 128 );
+		explicit_bzero( b2, 128 );
+	
+		return timingsafe_bcmp( b1, b2, 128 );
+	}
+
+You can validate this is correctly preventing optimisation by running
+something like `gcc -O3 -S foo.c` and examining the output in `foo.s`.
---
+++ safebfuns.h
@@ -0,0 +1,35 @@
+#include <string.h>
+
+#if __GNUC__ > 4 || ( __GNUC__ == 4 && __GNUC_MINOR__ > 4 )
+	// http://gcc.gnu.org/onlinedocs/gcc/Function-Specific-Option-Pragmas.html
+	// http://gcc.gnu.org/onlinedocs/gcc/Function-Attributes.html
+	#pragma GCC push_options
+	#pragma GCC optimize ( "-O0" )
+	#define NOINLINE __attribute__ (( noinline ))
+#else
+	#error "require gcc >= 4.4"
+#endif
+
+NOINLINE void explicit_bzero( void * const buf, const size_t n ) {
+	size_t i;
+	unsigned char * p = buf;
+
+	for( i = 0; i < n; i++ ) {
+		p[ i ] = 0;
+	}
+}
+
+NOINLINE int timingsafe_bcmp( const void * const b1, const void * const b2, const size_t n ) {
+	size_t i;
+	const unsigned char * p1 = b1;
+	const unsigned char * p2 = b2;
+	int result = 0;
+
+	for( i = 0; i < n; i++ ) {
+		result |= p1[ i ] != p2[ i ];
+	}
+
+	return result;
+}
+
+#pragma GCC pop_options
