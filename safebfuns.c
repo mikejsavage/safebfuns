1d21e9b539d0eb91041a6df74434952bec4908c0
KovBal <kovacs.balazs.e@gmail.com>
2014-06-09 17:35:08 +0200

    Two small, but important changes: * separated definition / implementation -- useful for actual usage * use of xor instead of comparision in timingsafe_bcmp() -- see http://rdist.root.org/2010/01/07/timing-independent-array-comparison/ and https://lobste.rs/s/snlsqj/safe_explicit_bzero_timingsafe_bcmp_for_linux

--- safebfuns.c
+++ safebfuns.c
@@ -1,14 +1,13 @@
 /* Public domain */
 
-#ifndef SAFEBFUNS_H
-#define SAFEBFUNS_H
-
 #include <string.h>
 
 #if __clang__
 	// http://clang.llvm.org/docs/LanguageExtensions.html#feature-checking-macros
 	// http://lists.cs.uiuc.edu/pipermail/llvmdev/2013-April/061527.html
 	#if __has_attribute( noinline )
+		#pragma clang optimize push
+		#pragma clang optimize ( "O0" )
 		#define NOINLINE __attribute__ (( noinline ))
 	#else
 		#error "require clang that supports noinline"
@@ -17,6 +16,8 @@
 	// http://gcc.gnu.org/onlinedocs/gcc/Function-Specific-Option-Pragmas.html
 	// http://gcc.gnu.org/onlinedocs/gcc/Function-Attributes.html
 	#if __GNUC__ > 4 || ( __GNUC__ == 4 && __GNUC_MINOR__ > 4 )
+		#pragma GCC push_options
+		#pragma GCC optimize ( "-O0" )
 		#define NOINLINE __attribute__ (( noinline ))
 	#else
 		#error "require gcc >= 4.4"
@@ -25,8 +26,30 @@
 	#error "unrecognised compiler"
 #endif
 
-NOINLINE void explicit_bzero( void * const buf, const size_t n );
+NOINLINE void explicit_bzero( void * const buf, const size_t n ) {
+	size_t i;
+	unsigned char * p = buf;
 
-NOINLINE int timingsafe_bcmp( const void * const b1, const void * const b2, const size_t n );
+	for( i = 0; i < n; i++ ) {
+		p[ i ] = 0;
+	}
+}
 
-#endif // SAFEBFUNS_H
+NOINLINE int timingsafe_bcmp( const void * const b1, const void * const b2, const size_t n ) {
+	size_t i;
+	const unsigned char * const p1 = b1;
+	const unsigned char * const p2 = b2;
+	int result = 0;
+
+	for( i = 0; i < n; i++ ) {
+		result |= p1[ i ] ^ p2[ i ];
+	}
+
+	return result;
+}
+
+#ifdef __clang__
+	#pragma clang optimize pop
+#else
+	#pragma GCC pop_options
+#endif
