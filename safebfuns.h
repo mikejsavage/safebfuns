443c4667651c5a7e564b4df9e767e67e92f9357a
Michael Savage <mikejsavage@gmail.com>
2014-10-14 21:50:03 +0100

    Add timingsafe_memcmp

--- safebfuns.h
+++ safebfuns.h
@@ -28,5 +28,6 @@
 
 NOINLINE void explicit_bzero( void * const buf, const size_t n );
 NOINLINE int timingsafe_bcmp( const void * const b1, const void * const b2, const size_t n );
+NOINLINE int timingsafe_memcmp( const void * b1, const void * b2, const size_t len );
 
 #undef NOINLINE
