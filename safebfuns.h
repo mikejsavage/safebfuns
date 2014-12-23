3f09db7c9599e67498d5082b60a8078294111b5d
Michael Savage <mikejsavage@gmail.com>
2014-12-23 17:37:58 +0000

    Use function attributes instead of pragmas to disable optimisations
    Clang allows us to check if specific attributes have been implemented
    which is more robust than checking version numbers.
    
    GCC does not provide such functionality, but I changed the header guard
    to match for consistency.
    
    Also add a sentinel line as a further attempt to prevent accidental
    builds with unsupported compilers.

--- safebfuns.h
+++ safebfuns.h
@@ -5,12 +5,13 @@
 #if __clang__
 	/*
 	 * http://clang.llvm.org/docs/LanguageExtensions.html#feature-checking-macros
-	 * http://lists.cs.uiuc.edu/pipermail/llvmdev/2013-April/061527.html
+	 * http://lists.cs.uiuc.edu/pipermail/cfe-dev/2014-December/040627.html
 	 */
 	#if __has_attribute( noinline )
+		#define NOOPT __attribute__ (( optnone ))
 		#define NOINLINE __attribute__ (( noinline ))
 	#else
-		#error "require clang that supports noinline"
+		#error "require clang with noinline attribute"
 	#endif
 #elif __GNUC__
 	/*
@@ -18,16 +19,19 @@
 	 * http://gcc.gnu.org/onlinedocs/gcc/Function-Attributes.html
 	 */
 	#if __GNUC__ > 4 || ( __GNUC__ == 4 && __GNUC_MINOR__ > 4 )
+		#define NOOPT __attribute__ (( optimize( 0 ) ))
 		#define NOINLINE __attribute__ (( noinline ))
 	#else
 		#error "require gcc >= 4.4"
 	#endif
 #else
 	#error "unrecognised compiler"
+	explode
 #endif
 
-NOINLINE void explicit_bzero( void * const buf, const size_t n );
-NOINLINE int timingsafe_bcmp( const void * const b1, const void * const b2, const size_t n );
-NOINLINE int timingsafe_memcmp( const void * const b1, const void * const b2, const size_t len );
+NOOPT NOINLINE void explicit_bzero( void * const buf, const size_t n );
+NOOPT NOINLINE int timingsafe_bcmp( const void * const b1, const void * const b2, const size_t n );
+NOOPT NOINLINE int timingsafe_memcmp( const void * const b1, const void * const b2, const size_t len );
 
+#undef NOOPT
 #undef NOINLINE
